2e5545fec094817a26154ce2e3b2e0e4b2c6dc1d
gowoon-choi <gowoonvv@gmail.com>
2021-03-14 01:38:59 +0900

    실습1 : 실습 베이스 구축

---
+++ Practice1/tests/com/gowoon/MainTest.java
@@ -0,0 +1,14 @@
+package com.gowoon;
+
+import org.junit.*;
+
+public class MainTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addRecipe() {
+    }
+}
